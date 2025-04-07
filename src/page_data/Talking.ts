75c8e25b8c8cc54aaaa4ca71f3bb23ba3eb9fdae
UF4OVER <3397499417@qq.com>
2025-04-07 19:11:18 +0800

    refactor(page_data): 更新 Talking 页面数据
    - 修改 API 请求地址，从 talkingsapi.uf4.workers.dev 更改为 api.uf4.top- 在静态数据中添加一条新消息

--- src/page_data/Talking.ts
+++ src/page_data/Talking.ts
@@ -1,7 +1,7 @@
 export default
 {
   // API 接口请求优先，数据格式保持和 data 一致
-  api: 'https://talkingsapi.uf4.workers.dev/parse?url=https://sharechain.qq.com/1bef6cf8223f3bf287d137f6e6bdceb7',
+  api: 'https://api.uf4.top/parse?url=https://sharechain.qq.com/1bef6cf8223f3bf287d137f6e6bdceb7',
   // api 为空则使用 data 静态数据 
   // 注意：图片请用 vh-img-flex 类包裹
   data: [
@@ -9,6 +9,14 @@ export default
       "date": "2025-04-07 11:06:00",
       "tags": [
           "老姐",
+        "好美"
+      ],
+      "content": "老姐好美的<p class=\"vh-img-flex\"><img src=\"https://zycs-img-c5u.pages.dev/v2/poHm7cC.jpeg\"></p>"
+    },
+    {
+      "date": "2025-04-07 11:06:00",
+      "tags": [
+        "老姐",
         "好美"
       ],
       "content": "老姐好美的<p class=\"vh-img-flex\"><img src=\"https://zycs-img-c5u.pages.dev/v2/poHm7cC.jpeg\"></p>"
