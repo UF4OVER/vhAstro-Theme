90e4c69dd5a9c0a23cf883c987da093de68967eb
UF4OVER <3397499417@qq.com>
2025-04-07 18:39:08 +0800

    refactor(page_data): 更新 Talking 页面数据
    - 修改日期和时间- 更新图片链接
    -调整数据格式和缩进

--- src/page_data/Talking.ts
+++ src/page_data/Talking.ts
@@ -1,17 +1,17 @@
-export default {
+export default
+{
   // API 接口请求优先，数据格式保持和 data 一致
-  api: '',
+  api: 'https://talkingsapi.uf4.workers.dev/parse?url=https://sharechain.qq.com/1bef6cf8223f3bf287d137f6e6bdceb7',
   // api 为空则使用 data 静态数据 
   // 注意：图片请用 vh-img-flex 类包裹
   data: [
     {
-      "date": "2025-04-3 19:36:16",
+      "date": "2025-04-07 11:06:00",
       "tags": [
-        "老姐",
+          "老姐",
         "好美"
       ],
       "content": "老姐好美的<p class=\"vh-img-flex\"><img src=\"https://zycs-img-c5u.pages.dev/v2/poHm7cC.jpeg\"></p>"
-    },
-
+    }
   ]
 }
